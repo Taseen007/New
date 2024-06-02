4eb66375ac9e2b1a540a3a1d39054b1b6ef52b3a
Taseen007 <taseentajwar95@gmail.com>
2024-06-02 13:22:47 +0600

    New input

--- LabFinalTask2/src/Bedroom.java
+++ LabFinalTask2/src/Bedroom.java
@@ -1,8 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class Bedroom  extends Room {
 
     private String Light;
     private Double Thermostat;
     private String Speaker;
+
+
+    int age;
+
 
 
 }
